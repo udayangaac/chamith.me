6564db3cf17dcdddd1ffb45fcefe219f0fd25522
Chamith Udayanga <udayangaac@gmail.com>
2025-12-24 23:58:35 +0530

    Change fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+  weight: ["100","200","300","400","500","600","700","800","900"],
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,7 +42,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${montserrat.variable} ${montserrat.variable} antialiased`}
       >
         <Navbar/>
         {children}
